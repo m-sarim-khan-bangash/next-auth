afbd6e456930c8356c4114a437f7f638ab82fda6
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2025-07-28 19:39:37 +0500

    next-auth

---
+++ src/lib/actions/auth.js
@@ -0,0 +1,11 @@
+"use server";
+
+import { signIn, signOut } from "@/auth";
+
+export const login = async () => {
+  await signIn("github", { redirect: "/" });
+};
+
+export const logout = async () => {
+  await signOut({ redirect: "/" });
+};
